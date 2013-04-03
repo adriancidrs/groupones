eea47e8a64076f22cd93de3aee00a7d090fff7e8
Adrián Cid Ramos <adrian.cid.rs@gmail.com>
2013-04-03 09:31:23 +0200

    Añadido params

--- conexion.py
+++ conexion.py
@@ -1,4 +1,5 @@
 import requests
-r=requests.get("http://api.groupon.com/v2/channels/getaways/deals.xml?client_id=eb9ff18a490c1d0e00419123560879fa1b16e02d&show=title,status,largeImageUrl,mediumImageUrl,isTipped,tippingPoint,endAt,dealUrl,merchant,isSoldOut,tippedAt,soldQuantity,options,announcementTitle,tags HTTP/1.1")
-print r.url
-r.text
+r=requests.get("https://api.groupon.com/v2/deals.json", params = {"client_id":"eb9ff18a490c1d0e00419123560879fa1b16e02d","division_id":"san-francisco"})
+print r.text
+
+
